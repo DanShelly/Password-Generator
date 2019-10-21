bcb9a5bf99c886a47c156149491314783b73cb5b
Dan Shelly <djshelly24@gmail.com>
2019-10-21 07:16:46 -0400

    Created prompts and alerts for gathering information about the password, along with if statements if information is invalid

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
-
-
 //Confirm that the user needs a password
 var needPassword = confirm("Do you need a password?");
 alert("You need a password: " + needPassword);
 
+//Password array
+var genPass = [];
+
+
 //Prompt preferred length of password
 if (needPassword) {
-var passLength = prompt("How long would you like the password to be? Must be a minimum of 8 characters with a maximum of 128.");
-    if (passLength < 8) {
-        alert("Password must be more than 8 characters, try again!");
-        var passLength = prompt("How long would you like the password to be? Must be a minimum of 8 characters with a maximum of 128.");
-    }
-    else if (passLength > 128) {
-        alert("Password must be less than 128 characters, try again!");
-        var passLength = prompt("How long would you like the password to be? Must be a minimum of 8 characters with a maximum of 128.");
+
+    //Need a for loop to keep running this if they continue to select numbers outside the range....right?
+    var passLength = prompt("How long would you like the password to be? Must be a minimum of 8 characters or a maximum of 128.");
+    if (passLength < 8 || passLength > 128) {
+        alert("Password must be between 8 and 128 characters, try again!");
+        var passLength = prompt("How long would you like the password to be? Must be a minimum of 8 characters or a maximum of 128.");
     }
     else {
         alert("You'd like your password to be " + passLength + " characters.");
+        genPass.push(passLength);
     }
 
+    //Prompt special characters
+    var wantSpecChar = confirm("Do you want to use a special character?");
+    if (wantSpecChar) {
+        var specChar = prompt("What special character would you like to use?"); //How do I get " special character to work? And backslash
+        if (specChar === " " || specChar === "!" || specChar === " " || specChar === "#" || specChar === "$" || specChar === "%" || specChar === "&" || specChar === "'" || specChar === "(" || specChar === ")" || specChar === "*" || specChar === "+" || specChar === "," || specChar === "-" || specChar === "." || specChar === "/" || specChar === ":" || specChar === ";" || specChar === "<" || specChar === "=" || specChar === ">" || specChar === "?" || specChar === "@" || specChar === "[" || specChar === " " || specChar === "]" || specChar === "^" || specChar === "_" || specChar === "`" || specChar === "{" || specChar === "|" || specChar === "}" || specChar === "~") {
+            genPass.push(specChar);
+        }
+        else {
+            alert("Not a special character, try again!");
+            var specChar = prompt("What special character would you like to use?"); //How do I get " special character to work? And backslash
+            if (specChar === " " || specChar === "!" || specChar === " " || specChar === "#" || specChar === "$" || specChar === "%" || specChar === "&" || specChar === "'" || specChar === "(" || specChar === ")" || specChar === "*" || specChar === "+" || specChar === "," || specChar === "-" || specChar === "." || specChar === "/" || specChar === ":" || specChar === ";" || specChar === "<" || specChar === "=" || specChar === ">" || specChar === "?" || specChar === "@" || specChar === "[" || specChar === " " || specChar === "]" || specChar === "^" || specChar === "_" || specChar === "`" || specChar === "{" || specChar === "|" || specChar === "}" || specChar === "~") {
+                genPass.push(specChar);
+            }
+
+        }
+
+    }
+
+    //Prompt numeric characters
+    var wantNumChar = confirm("Do you want to use a numeric character?");
+    if (wantNumChar) {
+        var numChar = prompt("What number do you want to use?");
+        if (Number.isInteger(numChar)) {
+            genPass.push(numChar);
+        }
+        else {
+            alert("Not a number, try again!");
+            var numChar = prompt("What number do you want to use?");
+            if (Number.isInteger(numChar)) {
+                genPass.push(numChar);
+            }
+        }
 
 
-    
+    }
+
+    //Prompt lowercase characters
+    var wantLowCase = confirm("Do you want to use a lowercase letter?");
+    if (wantLowCase) {
+        var lowCase = prompt("What lowercase letter do you want to use?");
+
+    }
+
+    //Prompt uppercase characters
+    var wantUpperCase = confirm("Do you want to use a uppercase letter?");
+    if (wantUpperCase) {
+        var upperCase = prompt("What uppercase letter do you want to use?");
+
+    }
+
+}
 
 
-//Character type prompts
-//Prompt if using special characters
-//var specChar = prompt("Do you want to use special characters?");
-//alert("You'd like your password to be " + passLength + " characters.");
-
-//Prompt if using numeric characters
-//var numChar = prompt("Do you want to use numerical characters?");
-//alert("You'd like your password to be " + passLength + " characters.");
-
-//Prompt if using lowercase characters
-//var lowChar = prompt("Do you want to use lowercase characters?");
-//alert("You'd like your password to be " + passLength + " characters.");
-
-//Prompt if using uppercase characters
-//var upChar = prompt("Do you want to use uppercase characters?");
-//alert("You'd like your password to be " + passLength + " characters.");
-
-//Generated password
-//var genPass = 
-
-
-//if (needPassword) 
+//function generate_random_pass {
+//
